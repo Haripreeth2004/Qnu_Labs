da635d5fb99cf5cbb7aa9b4de50f86552cf6e6b1
Haripreeth2004 <122097149+Haripreeth2004@users.noreply.github.com>
2024-06-10 17:09:08 +0530

    Add files via upload

---
+++ adder (1).py
@@ -0,0 +1,43 @@
+from myhdl import block, always_comb, Signal, intbv, delay, instance, Simulation, toVHDL, ResetSignal
+
+@block
+def adder(a, b, c):
+    @always_comb
+    def logic():
+        c.next = a + b
+    return logic
+
+@block
+def testbench():
+    a = Signal(intbv(0)[4:])
+    b = Signal(intbv(0)[4:])
+    c = Signal(intbv(0)[5:])  # Extra bit for overflow
+
+    adder_inst = adder(a, b, c)
+
+    @instance
+    def stimulus():
+        for i in range(4):
+            for j in range(4):
+                a.next = i
+                b.next = j
+                yield delay(1000)
+                print(f"a = {a}, b = {b}, c = {c}")
+
+    return adder_inst, stimulus
+
+# Run the simulation
+tb = testbench()
+sim = Simulation(tb)
+sim.run()
+
+# Convert the adder to VHDL
+a = Signal(intbv(0)[4:])
+b = Signal(intbv(0)[4:])
+c = Signal(intbv(0)[5:])
+adder_inst = adder(a, b, c)
+toVHDL(adder_inst, a, b, c)
+
+# Convert the testbench to VHDL
+tb = testbench()
+tb.convert(hdl='VHDL')
